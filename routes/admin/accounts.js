c2f9e926b26a810829af418d74a16ff2d274e927
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-24 15:58:15 -0500

    Updates accounts admin functions

--- routes/admin/accounts.js
+++ routes/admin/accounts.js
@@ -15,15 +15,13 @@ function deleteAccount(id) {
 function getAccounts() {
   return new Promise(async (resolve, reject) => {
     let accounts, role_options, mergedFields;
-    await db.transaction(async t => {
-      try {
+    try {
+      await db.transaction(async t => {
         accounts = await db("accounts")
           .select(
             "accounts.id",
-            "accounts.email",
             "accounts.first_name",
             "accounts.last_name",
-            "accounts.verified_student",
             "accounts.role_id",
             "roles.role"
           )
@@ -33,16 +31,15 @@ function getAccounts() {
         role_options = await db("roles")
           .select("id as role_id", "role")
           .transacting(t);
-      } catch (error) {
-        t.rollback();
-        reject(error);
-      }
-    });
-    mergedFields = accounts.map(account => ({
-      ...account,
-      role_options
-    }));
-    resolve(mergedFields);
+      });
+      mergedFields = accounts.map(account => ({
+        ...account,
+        role_options
+      }));
+      resolve(mergedFields);
+    } catch (error) {
+      reject(error);
+    }
   });
 }
 
@@ -51,14 +48,7 @@ function updateAccount(id, info) {
     try {
       const [account] = await db("accounts")
         .where({ id })
-        .update(info, [
-          "id",
-          "email",
-          "first_name",
-          "last_name",
-          "verified_student",
-          "role_id"
-        ]);
+        .update(info, ["id", "first_name", "last_name", "role_id"]);
       resolve(account);
     } catch (error) {
       reject(error);
