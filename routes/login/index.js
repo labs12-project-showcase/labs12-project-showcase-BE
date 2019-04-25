4d4d6fbae90d7184b4b8496c20b32bd895843a5b
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-25 15:55:08 -0500

    Implements first try at Auth0

--- routes/login/index.js
+++ routes/login/index.js
@@ -4,7 +4,17 @@ const actions = require("./login");
 const generateToken = require("../../auth/token-handlers").generateToken;
 
 router.route("/").post(async (req, res) => {
+  const info = req.body;
   try {
+    const user = await actions.findUser(info.sub_id);
+    if (user) {
+      const token = generateToken(user);
+      res.status(200).json(token);
+    } else {
+      const [newUser] = await actions.registerUser(info);
+      const token = generateToken(newUser);
+      res.status(201).json(token);
+    }
   } catch (err) {
     res.status(500).json({ message: "Something went wrong logging in." });
   }
--- routes/login/login.js
+++ routes/login/login.js
@@ -1,13 +1,18 @@
 const db = require("../../data/config");
 
 module.exports = {
-  findUser
+  findUser,
+  registerUser
 };
 
-async function findUser(email) {
+function findUser(sub_id) {
   return db("accounts")
     .select("accounts.*", "roles.role")
     .innerJoin("roles", "accounts.role_id", "roles.id")
-    .where({ "accounts.email": email })
+    .where({ "accounts.sub_id": sub_id })
     .first();
+}
+
+function registerUser(info) {
+  return db("accounts").insert(info, "*");
 }
