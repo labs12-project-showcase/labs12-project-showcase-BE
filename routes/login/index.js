a16f8374c8c51187d416db8be8d1bf50bb009f2d
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-25 16:21:42 -0500

    Fixes seed issues, adds lots of console logs

--- routes/login/index.js
+++ routes/login/index.js
@@ -5,17 +5,22 @@ const generateToken = require("../../auth/token-handlers").generateToken;
 
 router.route("/").post(async (req, res) => {
   const info = req.body;
+  console.log("INFO IN REQ", info);
   try {
     const user = await actions.findUser(info.sub_id);
     if (user) {
+      console.log("User exists");
       const token = generateToken(user);
       res.status(200).json(token);
     } else {
+      console.log("Try adding user");
       const [newUser] = await actions.registerUser(info);
+      console.log("Made new user", newUser);
       const token = generateToken(newUser);
       res.status(201).json(token);
     }
   } catch (err) {
+    console.log("ERROR", err);
     res.status(500).json({ message: "Something went wrong logging in." });
   }
 });
