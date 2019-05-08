d9e8e38803dcabbc632f3ea47f45ff8781bdaf04
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-08 16:58:01 -0400

    Updates 'getProjects()' function in projects.js file to pull live data in from back end to Admin's project table

--- routes/admin/projects.js
+++ routes/admin/projects.js
@@ -27,30 +27,7 @@ function updateProject(id, info) {
     });
 }
 
-// function getProjects() {
-//     return new Promise(async (resolve, reject) => {
-//       try {
-//         let projects = await db("projects as p")
-//           .select(
-//             "p.name",
-//             "p.short_description",
-//             "s.id",
-//             "p.approved",
-//             "p.id"
-//           )
-//           .innerJoin("student_projects as sp", "p.id", "sp.project_id")
-//           .leftOuterJoin("students as s", "s.id", "sp.student_id");
-//         // .innerJoin("student_projects as sp", "p.id", "sp.project_id");
-//         //   .leftOuterJoin("tracks as t", "t.id", "s.track_id")
-//         //   .leftOuterJoin("cohorts as c", "c.id", "s.cohort_id");
-  
-//         resolve(projects);
-//       } catch (error) {
-//         console.log(error);
-//         reject(error);
-//       }
-//     });
-// }
+
 
 
 function getProjects() {
@@ -61,9 +38,13 @@ function getProjects() {
           "p.id",
           "p.name",
           "p.short_description",
-          db.raw("array_agg(distinct sp.student_id) as student_ids")
+          "p.approved",
+          db.raw("array_agg(distinct sp.student_id) as student_ids"),
+          db.raw("array_agg(distinct a.name) as students")
         )
         .leftOuterJoin("student_projects as sp", "sp.project_id", "p.id")
+        .leftOuterJoin("students as s", "s.id", "sp.student_id")
+        .leftOuterJoin("accounts as a", "a.id", "s.id")
         .groupBy("p.id");
 
       resolve(projects);
@@ -77,19 +58,21 @@ function getProjects() {
 
 // function getProjects() {
 //   return new Promise(async (resolve, reject) => {
-//     let projects, students;
+//     let project, students;
 //     try {
 //       await db.transaction(async t => {
 //         projects = await db("projects as p")
 //           .select(
-//             "p.*",
-//             db.raw("array_agg(distinct ps.skill) as project_skills"),
-//             db.raw("array_agg(distinct pm.media) as project_media")
+//             "p.id",
+//             "p.name",
+//             "p.short_description",
+//             "p.approved",
+//             db.raw("array_agg(distinct sp.student_id) as student_ids"),
+//             db.raw("array_agg(distinct a.name) as students")
 //           )
-//           .leftOuterJoin("project_media as pm", "pm.project_id", "p.id")
-//           .leftOuterJoin("project_skills as ps", "ps.project_id", "p.id")
-//           // .where({ "p.id": id })
-//           .first()
+//           .leftOuterJoin("student_projects as sp", "sp.project_id", "p.id")
+//           .leftOuterJoin("students as s", "s.id", "sp.student_id")
+//           .leftOuterJoin("accounts as a", "a.id", "s.id")
 //           .groupBy("p.id")
 //           .transacting(t);
 
@@ -97,14 +80,12 @@ function getProjects() {
 //           .select("s.profile_pic", "a.name", "s.id as student_id")
 //           .join("students as s", "s.id", "sp.student_id")
 //           .join("accounts as a", "a.id", "s.account_id")
-//           // .where({ "sp.project_id": project.id })
 //           .transacting(t);
 
 //         const top_students = await db("top_projects as tp")
 //           .select("s.profile_pic", "a.name", "s.id as student_id")
 //           .join("students as s", "s.id", "tp.student_id")
 //           .join("accounts as a", "a.id", "s.account_id")
-//           // .where({ "tp.project_id": project.id })
 //           .transacting(t);
 
 //         students = [...students, ...top_students];
