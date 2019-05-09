879555973864c30bbb72e8e046a51b9ab4168286
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-09 14:17:58 -0400

    Creates a .map() function inside of another .map() function to create an object inside each project that contains a student's id and their name on an associated project

--- routes/admin/projects.js
+++ routes/admin/projects.js
@@ -28,6 +28,45 @@ function updateProject(id, info) {
 }
 
 
+// function getProjects() {
+//   return new Promise(async (resolve, reject) => {
+//     let projects, students;
+//     try {
+//       await db.transaction(async t => {
+//         projects = await db("projects as p")
+//           .select(
+//             "p.*",
+//             db.raw("array_agg(distinct ps.skill) as project_skills"),
+//             db.raw("array_agg(distinct pm.media) as project_media")
+//           )
+//           .leftOuterJoin("project_media as pm", "pm.project_id", "p.id")
+//           .leftOuterJoin("project_skills as ps", "ps.project_id", "p.id")
+//           .groupBy("p.id")
+//           .transacting(t);
+
+//         students = await db("student_projects as sp")
+//           .select("s.profile_pic", "a.name", "s.id as student_id")
+//           .join("students as s", "s.id", "sp.student_id")
+//           .join("accounts as a", "a.id", "s.account_id")
+//           .transacting(t);
+
+//         const top_students = await db("top_projects as tp")
+//           .select("s.profile_pic", "a.name", "s.id as student_id")
+//           .join("students as s", "s.id", "tp.student_id")
+//           .join("accounts as a", "a.id", "s.account_id")
+//           .transacting(t);
+
+//         students = [...students, ...top_students];
+//       });
+//       resolve({ ...projects, students });
+//     } catch (error) {
+//       console.log(error);
+//       reject(error);
+//     }
+//   });
+// }
+
+
 
 
 function getProjects() {
@@ -47,7 +86,17 @@ function getProjects() {
         .leftOuterJoin("accounts as a", "a.id", "s.id")
         .groupBy("p.id");
 
-      resolve(projects);
+      resolve(projects.map(project => {
+        return {
+          ...project, 
+          students: project.students.map((student, index) => {
+          return {
+            name: student,
+            student_id: project.student_ids[index]
+          }
+        })}
+        }
+      ))
     } catch (error) {
       console.log(error);
       reject(error);
