020d620b0f1b8c4a1a9130b735e6fc43d037f7cc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-09 16:42:47 -0400

    Removes unnecessary commented code

--- routes/admin/projects.js
+++ routes/admin/projects.js
@@ -28,47 +28,6 @@ function updateProject(id, info) {
 }
 
 
-// function getProjects() {
-//   return new Promise(async (resolve, reject) => {
-//     let projects, students;
-//     try {
-//       await db.transaction(async t => {
-//         projects = await db("projects as p")
-//           .select(
-//             "p.*",
-//             db.raw("array_agg(distinct ps.skill) as project_skills"),
-//             db.raw("array_agg(distinct pm.media) as project_media")
-//           )
-//           .leftOuterJoin("project_media as pm", "pm.project_id", "p.id")
-//           .leftOuterJoin("project_skills as ps", "ps.project_id", "p.id")
-//           .groupBy("p.id")
-//           .transacting(t);
-
-//         students = await db("student_projects as sp")
-//           .select("s.profile_pic", "a.name", "s.id as student_id")
-//           .join("students as s", "s.id", "sp.student_id")
-//           .join("accounts as a", "a.id", "s.account_id")
-//           .transacting(t);
-
-//         const top_students = await db("top_projects as tp")
-//           .select("s.profile_pic", "a.name", "s.id as student_id")
-//           .join("students as s", "s.id", "tp.student_id")
-//           .join("accounts as a", "a.id", "s.account_id")
-//           .transacting(t);
-
-//         students = [...students, ...top_students];
-//       });
-//       resolve({ ...projects, students });
-//     } catch (error) {
-//       console.log(error);
-//       reject(error);
-//     }
-//   });
-// }
-
-
-
-
 function getProjects() {
   return new Promise(async (resolve, reject) => {
     try {
