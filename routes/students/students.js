3630fab6461161b34bef8bbb8eb1e36c0f70c006
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-14 17:23:53 -0500

    Adds location to student card queries

--- routes/students/students.js
+++ routes/students/students.js
@@ -118,7 +118,7 @@ function getFilteredStudentCards({
   lat = null,
   lon = null,
   tracks,
-  within = null,
+  within = null
 }) {
   // console.log('queries', tracks, badge, within);
   let trackString = "and (";
@@ -140,21 +140,23 @@ function getFilteredStudentCards({
         `select
           s.id,
           a.name,
-          s.linkedin,
-          s.github,
-          s.twitter,
+          s.location,
           s.profile_pic,
           s.lat,
           s.lon,
           t.name as track,
           array_agg(distinct ts.skill) as top_skills,
           jsonb_agg(distinct jsonb_build_object('name', p.name, 'project_id', p.id, 'media', pm.media)) as top_projects,
-          ${ filterDesLoc === "true" ? "jsonb_agg(distinct jsonb_build_object('lat', dl.lat, 'location', dl.location, 'lon', dl.lon)) as desired_locations" : null}
+          ${
+            filterDesLoc === "true"
+              ? "jsonb_agg(distinct jsonb_build_object('lat', dl.lat, 'location', dl.location, 'lon', dl.lon)) as desired_locations"
+              : null
+          }
           from accounts as a
           inner join students as s on s.account_id = a.id
           and approved = true
-          ${ badge === "true" ? "and acclaim != '' and acclaim is not null" : ''}
-          ${ tracks === 'none' ? '' : `${trackString}` }
+          ${badge === "true" ? "and acclaim != '' and acclaim is not null" : ""}
+          ${tracks === "none" ? "" : `${trackString}`}
           left outer join tracks as t on s.track_id = t.id
           left outer join top_skills as ts on s.id = ts.student_id
           left outer join top_projects as tp on tp.student_id = s.id
@@ -179,7 +181,7 @@ function getFilteredStudentCards({
           lat,
           lon,
           within,
-          filterDesLoc,
+          filterDesLoc
         );
         resolve(studentsFilteredByLocation);
       } else {
@@ -196,7 +198,7 @@ function getStudentCards() {
   return new Promise(async (resolve, reject) => {
     try {
       const { rows: students } = await db.raw(
-        "select s.id, a.name, s.linkedin, s.github, s.twitter, s.profile_pic, t.name as track, array_agg(distinct ts.skill) as top_skills, jsonb_agg(distinct jsonb_build_object('name', p.name, 'project_id', p.id, 'media', pm.media)) as top_projects from accounts as a inner join students as s on s.account_id = a.id and approved = true left outer join tracks as t on s.track_id = t.id left outer join top_skills as ts on s.id = ts.student_id left outer join top_projects as tp on tp.student_id = s.id left outer join projects as p on p.id = tp.project_id left outer join project_media as pm on pm.id = ( select project_media.id from project_media where project_media.project_id = p.id limit 1)group by s.id, a.name, s.linkedin, s.github, s.twitter, s.profile_pic, t.name"
+        "select s.id, a.name, s.location, s.profile_pic, t.name as track, array_agg(distinct ts.skill) as top_skills, jsonb_agg(distinct jsonb_build_object('name', p.name, 'project_id', p.id, 'media', pm.media)) as top_projects from accounts as a inner join students as s on s.account_id = a.id and approved = true left outer join tracks as t on s.track_id = t.id left outer join top_skills as ts on s.id = ts.student_id left outer join top_projects as tp on tp.student_id = s.id left outer join projects as p on p.id = tp.project_id left outer join project_media as pm on pm.id = ( select project_media.id from project_media where project_media.project_id = p.id limit 1)group by s.id, a.name, s.linkedin, s.github, s.twitter, s.profile_pic, t.name"
       );
       resolve(students);
     } catch (error) {
